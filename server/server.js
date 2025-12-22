44f9dc16c77b4227754573d4263c0cffd6dadbeb
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2025-12-22 19:30:06 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cookieParser());
 // app.use(cors({origin: 'http://localhost:5173', credentials: true}));
-app.use(cors({origin: 'https://authfullstack-dwoc.onrender.com', credentials: true}));
+app.use(cors({origin: 'https://authfullstack-5q55.onrender.com', credentials: true}));
 app.use('/api/auth', authRouter);
 app.use('/api/settings', settingsRouter);
 app.use('/api/Dashboard', homeRouter);
