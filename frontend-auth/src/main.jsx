8aad1ec8ceaf199e20ec714b968ac0646e202863
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2026-01-20 21:50:41 +0800

    dev branch

--- frontend-auth/src/main.jsx
+++ frontend-auth/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App.jsx';
 import { AppContextProvider } from './components/AppContext.jsx';
-
+//dev
 createRoot(document.getElementById('root')).render(
   <StrictMode>
      <BrowserRouter>
